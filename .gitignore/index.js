c22150e03f13263320d69ac14268a6844742e551
dhaorixx <56195254+dhaorixx@users.noreply.github.com>
2019-11-15 20:59:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,23 +12,7 @@ var opus = require('opusscript');
 
 var prefix = "$"
 
-try {
-function changing_status() {
-    const status = [
-      `$help | ${bot.guilds.size} serveurs`,
-      `$maj | Dernière nouveaté`,
-      `$help | ${bot.users.size} utilisateus`,
-   
-    ];
-    const random = status[Math.floor(Math.random() * status.length)];
-    bot.user.setActivity(random, {
-        type: "WATCHING"
-    });
-}
-setInterval(changing_status, 20000);
-} catch (error) {
-console.log(error)
-}
+
 
 
 
